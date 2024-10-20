e8d209e47014b7d37b0ce5b158861da88fd12a14
J Abraham <james.abraham@aqovia.com>
2024-10-20 23:46:57 +0100

    Comment out UseHttpRedirection

--- TestDotNetDroplet/TestDotNetDroplet/Program.cs
+++ TestDotNetDroplet/TestDotNetDroplet/Program.cs
@@ -13,7 +13,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+app.UseForwardedHeaders();
 app.UseStaticFiles();
 
 app.UseRouting();
